654ad3130cea61d1cc76cf20b1e163fce21c3cee
LmBlueJay <66698888+LmBlueJay@users.noreply.github.com>
2020-06-10 11:23:10 -0400

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings: #Stores all game settings
+
+    def __init__(self): # initialize settings
+        # Screen
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
